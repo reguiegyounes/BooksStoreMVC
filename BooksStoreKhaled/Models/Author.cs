b4905fb1988fc699d50013e2cfa59bb7636ca33c
younes <reguiegyounes@gmail.com>
2021-06-06 14:04:48 +0100

    add models

---
+++ BooksStoreKhaled/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStoreKhaled.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string FullName { get; set; }
+    }
+}
