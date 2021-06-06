d167aed72c74827b6370e78ad3fb4dd331ba042e
younes <reguiegyounes@gmail.com>
2021-06-06 14:16:45 +0100

    add inteface IBooksStoreRepository<TEntity>

--- BooksStoreKhaled/Models/Book.cs
+++ BooksStoreKhaled/Models/Book.cs
@@ -7,5 +7,9 @@ namespace BooksStoreKhaled.Models
 {
     public class Book
     {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Author Author { get; set; }
     }
 }
---
+++ BooksStoreKhaled/Models/Repositories/IBooksStoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStoreKhaled.Models.Repositories
+{
+    interface IBooksStoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int? id);
+        void add(TEntity entity);
+        void update(TEntity entity);
+        void delete(int id);
+    }
+}
