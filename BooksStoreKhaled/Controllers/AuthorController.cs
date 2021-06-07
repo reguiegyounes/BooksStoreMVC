9aadda2328f015f7130ebb06644a9d70b327b294
younes <reguiegyounes@gmail.com>
2021-06-07 21:15:49 +0100

    create view Details of Author

--- BooksStoreKhaled/Controllers/AuthorController.cs
+++ BooksStoreKhaled/Controllers/AuthorController.cs
@@ -28,7 +28,8 @@ namespace BooksStoreKhaled.Controllers
         // GET: AuthorController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var author = authorRepository.Find(id);
+            return View(author);
         }
 
         // GET: AuthorController/Create
