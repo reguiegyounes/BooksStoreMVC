a01e7b663c142854a40fdf34c65743145513a6c4
younes <reguiegyounes@gmail.com>
2021-06-09 15:19:44 +0100

    add view editAuthor

--- BooksStoreKhaled/Controllers/AuthorController.cs
+++ BooksStoreKhaled/Controllers/AuthorController.cs
@@ -57,16 +57,18 @@ namespace BooksStoreKhaled.Controllers
         // GET: AuthorController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var author = authorRepository.Find(id);
+            return View(author);
         }
 
         // POST: AuthorController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Author author)
         {
             try
             {
+                authorRepository.update(id,author);
                 return RedirectToAction(nameof(Index));
             }
             catch
