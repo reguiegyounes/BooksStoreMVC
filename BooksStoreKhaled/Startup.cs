def1da3e9d340cfa031636eeb2265b41e56a66dd
younes <reguiegyounes@gmail.com>
2021-06-09 14:51:47 +0100

    add static files and layout

--- BooksStoreKhaled/Startup.cs
+++ BooksStoreKhaled/Startup.cs
@@ -28,6 +28,7 @@ namespace BooksStoreKhaled
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
