4beaa4e73e2f197e9899dc200b227049037c13ba
younes <reguiegyounes@gmail.com>
2021-06-09 15:51:43 +0100

    create view getAllBooks

---
+++ BooksStoreKhaled/Controllers/BookController.cs
@@ -0,0 +1,96 @@
+﻿using BooksStoreKhaled.Models;
+using BooksStoreKhaled.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStoreKhaled.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBooksStoreRepository<Book> bookRepository;
+
+        public BookController(IBooksStoreRepository<Book> bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        // GET: BookController
+        public ActionResult Index()
+        {
+            var books = bookRepository.List();
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
