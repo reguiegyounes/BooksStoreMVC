4c05e156769ae88b958850d6975a520ed18d6958
younes <reguiegyounes@gmail.com>
2021-06-18 15:51:24 +0100

    add databaseContext

---
+++ BooksStoreKhaled/Models/StoreBooksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStoreKhaled.Models
+{
+    public class StoreBooksDbContext : DbContext
+    {
+        public StoreBooksDbContext(DbContextOptions<StoreBooksDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
