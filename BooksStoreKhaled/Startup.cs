69a8ebe9625b7eab66bc8c3387db200be95116e6
younes <reguiegyounes@gmail.com>
2021-06-18 16:48:55 +0100

    add connectionString of Database

--- BooksStoreKhaled/Startup.cs
+++ BooksStoreKhaled/Startup.cs
@@ -2,20 +2,22 @@ using BooksStoreKhaled.Models;
 using BooksStoreKhaled.Models.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BooksStoreKhaled
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(
@@ -23,6 +25,9 @@ namespace BooksStoreKhaled
             );
             services.AddSingleton<IBooksStoreRepository<Author>, cAuthorRepository>();
             services.AddSingleton<IBooksStoreRepository<Book>, cBookRepository>();
+            services.AddDbContext<StoreBooksDbContext>(options=> {
+                options.UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
